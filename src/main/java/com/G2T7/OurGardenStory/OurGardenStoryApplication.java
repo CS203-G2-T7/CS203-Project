2ae19f1fc7bea9dfb92db9cc485ae94db6306eaf
Sean Evan Seah <seanevanseah@gmail.com>
2022-09-14 15:57:21 +0800

    Initialised Spring Framework

---
+++ src/main/java/com/G2T7/OurGardenStory/OurGardenStoryApplication.java
@@ -0,0 +1,13 @@
+package com.G2T7.OurGardenStory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurGardenStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurGardenStoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/G2T7/OurGardenStory/OurGardenStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.G2T7.OurGardenStory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurGardenStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
