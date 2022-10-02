32548adba189a6fd22a6073c63bc6e13f8b639d6
Goh Wei Han <weihan.goh.2021@scis.smu.edu.sg>
2022-10-02 13:55:21 +0800

    added working geocoder

--- SpringBackend/src/main/java/com/G2T7/OurGardenStory/geocoder/Geocoder.java
+++ SpringBackend/src/main/java/com/G2T7/OurGardenStory/geocoder/Geocoder.java
@@ -8,23 +8,16 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 
-import org.springframework.beans.factory.annotation.Value;
-
 public class Geocoder {
 
     private static final String GEOCODING_RESOURCE = "https://geocode.search.hereapi.com/v1/geocode";
-    @Value("${Geocoder_API_KEY}")
-    private static String API_KEY; // Get from application.properties. Don't write in plaintext.
+    private static final String API_KEY = "sEul8xoZF2Wwg21RxDO5a0lxJJnsg0AWBetZeCHsZuI";
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        GeocodeSync(GEOCODING_RESOURCE);
-    }
-
-    public static String GeocodeSync(String query) throws IOException, InterruptedException {
+    public String GeocodeSync(String query) throws IOException, InterruptedException {
 
         HttpClient httpClient = HttpClient.newHttpClient();
 
-        String encodedQuery = URLEncoder.encode(query, "UTF-8");
+        String encodedQuery = URLEncoder.encode(query,"UTF-8");
         String requestUri = GEOCODING_RESOURCE + "?apiKey=" + API_KEY + "&q=" + encodedQuery;
 
         HttpRequest geocodingRequest = HttpRequest.newBuilder().GET().uri(URI.create(requestUri))
@@ -35,5 +28,4 @@ public class Geocoder {
 
         return (String) geocodingResponse.body();
     }
-
 }
--- SpringBackend/src/main/java/com/G2T7/OurGardenStory/geocoder/GeocodingExample.java
+++ SpringBackend/src/main/java/com/G2T7/OurGardenStory/geocoder/GeocodingExample.java
@@ -1,29 +1,83 @@
 package com.G2T7.OurGardenStory.geocoder;
 
 import java.io.IOException;
+import java.util.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GeocodingExample {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        String lat1 = "";
+        String lat2 = "";
+        String lng1 = "";
+        String lng2 = "";
 
         ObjectMapper mapper = new ObjectMapper();
         Geocoder geocoder = new Geocoder();
 
-        String response = geocoder.GeocodeSync("Blk 10 Stirling Road, S148954");
+        String response = geocoder.GeocodeSync("39F West Coast Park, S 127717");
         JsonNode responseJsonNode = mapper.readTree(response);
-
         JsonNode items = responseJsonNode.get("items");
+
+        String response2 = geocoder.GeocodeSync("Blk 10 Stirling Road, S 148954");
+        JsonNode responseJsonNode2 = mapper.readTree(response2);
+        JsonNode items2 = responseJsonNode2.get("items");
 
         for (JsonNode item : items) {
             JsonNode address = item.get("address");
             String label = address.get("label").asText();
             JsonNode position = item.get("position");
 
-            String lat = position.get("latitude").asText();
-            String lng = position.get("longitude").asText();
-            System.out.println(label + " is located at " + lat + "," + lng + ".");
+            lat1 = position.get("lat").asText();
+            lng1 = position.get("lng").asText();
+            System.out.println(label + " is located at " + lat1 + "," + lng1 + ".");
         }
+
+        for (JsonNode item : items2) {
+            JsonNode address = item.get("address");
+            String label = address.get("label").asText();
+            JsonNode position = item.get("position");
+
+            lat2 = position.get("lat").asText();
+            lng2 = position.get("lng").asText();
+            System.out.println(label + " is located at " + lat2 + "," + lng2 + ".");
+        }
+
+       double result = distanceBetweenTwoPoints(lat1, lng1, lat2, lng2);
+       System.out.println("The distance between the two addresses is " + result + "km");
+
+
+    }
+
+    public static double distanceBetweenTwoPoints(String lat1, String lng1, String lat2, String lng2) {
+        double latitude1 = Double.parseDouble(lat1);
+        double longitude1 = Double.parseDouble(lng1);
+        double latitude2 = Double.parseDouble(lat2);
+        double longitude2 = Double.parseDouble(lng2);
+
+        // The math module contains a function
+        // named toRadians which converts from
+        // degrees to radians.
+        longitude1 = Math.toRadians(longitude1);
+        longitude2 = Math.toRadians(longitude2);
+        latitude1 = Math.toRadians(latitude1);
+        latitude2 = Math.toRadians(latitude2);
+
+        // Haversine formula
+        double dlon = longitude1 - longitude2;
+        double dlat = latitude1 - latitude2;
+        double a = Math.pow(Math.sin(dlat / 2), 2)
+                + Math.cos(latitude1) * Math.cos(latitude2)
+                * Math.pow(Math.sin(dlon / 2),2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+
+        // Radius of earth in kilometers. Use 3956
+        // for miles
+        double r = 6371;
+
+        // calculate the result
+        return(c * r * 1.609344);
     }
 }
