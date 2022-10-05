74b06bc6076849b3ef9226b7b9243181d1d99c84
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2022-10-05 16:28:49 +0800

    Change flex.

--- ReactFrontend/src/pages/Home/Content/Table/Row/RowGardenEntry.styled.ts
+++ ReactFrontend/src/pages/Home/Content/Table/Row/RowGardenEntry.styled.ts
@@ -4,11 +4,10 @@ export const RowGardenEntryStyled = styled.div`
   color: #00131e;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   padding: 2.8125rem 1.923% 1.5rem;
   width: 36.378%;
   gap: 0.625rem;
-  /* min-height: 7.8125rem; */
   span:nth-child(2) {
     color: #9f9f9f;
   }
