b1765d978bf2606bdbf893f22311b9b933ec0812
Goh Wei Han <weihan.goh.2021@scis.smu.edu.sg>
2022-10-05 15:04:12 +0800

    refined geocode

--- SpringBackend/src/main/java/com/G2T7/OurGardenStory/geocoder/Geocoder.java
+++ SpringBackend/src/main/java/com/G2T7/OurGardenStory/geocoder/Geocoder.java
@@ -12,23 +12,13 @@ import org.springframework.beans.factory.annotation.Value;
 public class Geocoder {
 
     public String GeocodeSync(String query, String resource, String apiKey) throws IOException, InterruptedException {
-        // System.out.println("API_KEY = " + API_KEY);
         HttpClient httpClient = HttpClient.newHttpClient();
 
         String encodedQuery = URLEncoder.encode(query,"UTF-8");
         String requestUri = resource + "?apiKey=" + apiKey + "&q=" + encodedQuery;
-        URI uri = URI.create(requestUri);
-        System.out.println("request uri = " + requestUri);
-        System.out.println("uri = " + uri);
 
-        // HttpRequest geocodingRequest = HttpRequest.newBuilder().GET().uri(URI.create(requestUri))
-        //         .timeout(Duration.ofMillis(2000)).build();
-
-        HttpRequest.Builder geocodingRequest1 = HttpRequest.newBuilder().GET();
-        System.out.println(geocodingRequest1);
-        HttpRequest.Builder geocodingRequest2 = geocodingRequest1.uri(uri);
-        System.out.println(geocodingRequest2);
-        HttpRequest geocodingRequest = geocodingRequest2.timeout(Duration.ofMillis(2000)).build();
+        HttpRequest geocodingRequest = HttpRequest.newBuilder().GET().uri(URI.create(requestUri))
+                .timeout(Duration.ofMillis(2000)).build();
 
         HttpResponse geocodingResponse = httpClient.send(geocodingRequest,
                 HttpResponse.BodyHandlers.ofString());
