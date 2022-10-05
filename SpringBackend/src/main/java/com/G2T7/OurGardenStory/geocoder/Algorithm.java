b41b91c8175373fc6d3a4893d460b97024cfd3b2
Goh Wei Han <weihan.goh.2021@scis.smu.edu.sg>
2022-10-05 16:05:58 +0800

    added algo v1

---
+++ SpringBackend/src/main/java/com/G2T7/OurGardenStory/geocoder/Algorithm.java
@@ -0,0 +1,34 @@
+package com.G2T7.OurGardenStory.geocoder;
+
+import java.util.*;
+
+public class Algorithm {
+    
+    public ArrayList<String> getBallotSucess(HashMap<String,Double> ballotters, int numSuccess) {
+        ArrayList<String> list = new ArrayList<>(ballotters.keySet());
+        for (String key : ballotters.keySet()) {
+            if (ballotters.get(key) <= 2) {
+                list.add(key);
+                list.add(key);
+                list.add(key);
+                list.add(key);
+            }
+            if (ballotters.get(key) <= 5 && ballotters.get(key) > 2) {
+                list.add(key);
+                list.add(key);
+            }
+        }
+        ArrayList<String> output = new ArrayList<>();
+
+        for (int i = 0; i < numSuccess; i++) {
+            int size = list.size();
+            Random rand = new Random();
+            int idx = rand.nextInt(size);
+            output.add(list.get(idx));
+            list.removeIf(name -> name.equals(list.get(idx)));
+
+        }
+
+        return output;
+    }
+}
