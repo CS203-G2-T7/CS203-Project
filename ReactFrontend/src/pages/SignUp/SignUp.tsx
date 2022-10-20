59eefc7b03c3c16756fc43a5f2a3d6bef0f0a496
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2022-10-20 19:46:07 +0800

    Rename some frontend files.

--- ReactFrontend/src/pages/SignUp/SignUp.tsx
+++ ReactFrontend/src/pages/SignUp/SignUp.tsx
@@ -1,12 +1,12 @@
 import LeftContent from "./LeftContent/LeftContent";
-import RightContent from "./RightContent/RightContent";
+import SignUpFormComponent from "./RightContent/SignUpFormComponent";
 import { SignUpStyled } from "./SignUp.styled";
 
 export default function Login() {
   return (
     <SignUpStyled>
       <LeftContent />
-      <RightContent />
+      <SignUpFormComponent />
     </SignUpStyled>
   );
 }
