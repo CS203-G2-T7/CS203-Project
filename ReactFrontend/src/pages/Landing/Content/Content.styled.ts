e883d405eb516ceaf35835612c6214a99b6500bf
Wong Ginn Munn <ginnmunnwong@gmail.com>
2022-10-27 22:52:58 +0800

    included the carousel and its images

---
+++ ReactFrontend/src/pages/Landing/Content/Content.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ContentStyled = styled.div`
+  padding-top: 3rem;
+  width: 45%;
+  img {
+    width: 100%;
+  }
+`;
--- ReactFrontend/src/pages/Landing/Landing.tsx
+++ ReactFrontend/src/pages/Landing/Landing.tsx
@@ -2,6 +2,8 @@ import { LandingStyled } from "./Landing.styled";
 import { NavBarStyled } from "./NavBar.styled";
 import React from "react";
 import Navbar from "components/Navbar/Navbar";
+import Content from "./Content/Content";
+
 
 import {
   FacebookIcon,
@@ -15,13 +17,14 @@ import {
 type Props = {};
 
 
-export default function Login() {
+export default function Landing() {
   return (
     <LandingStyled>
 
       <NavBarStyled>
         <Navbar />
       </NavBarStyled>
+      <Content />
             <FacebookIcon />
             <InstagramIcon />
             <TwitterIcon />
