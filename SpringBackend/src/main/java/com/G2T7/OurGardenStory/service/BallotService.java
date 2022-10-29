00d71c2b23a923bbf9c5795389575da2f6ba622c
wahoong123 <choowh123@gmail.com>
2022-10-29 12:46:27 +0800

    ballot refactoring CRUD done

--- SpringBackend/src/main/java/com/G2T7/OurGardenStory/service/BallotService.java
+++ SpringBackend/src/main/java/com/G2T7/OurGardenStory/service/BallotService.java
@@ -2,8 +2,8 @@ package com.G2T7.OurGardenStory.service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
 
 import javax.management.relation.Relation;
 import javax.print.attribute.standard.MediaSize.ISO;
@@ -17,7 +17,10 @@ import com.G2T7.OurGardenStory.model.Garden;
 import com.G2T7.OurGardenStory.model.User;
 import com.G2T7.OurGardenStory.model.RelationshipModel.Ballot;
 import com.G2T7.OurGardenStory.model.RelationshipModel.Relationship;
+import com.G2T7.OurGardenStory.model.ReqResModel.UserSignInResponse;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.util.Base64;
+import org.springframework.util.StringUtils;
 import com.fasterxml.jackson.databind.JsonNode;
 
 @Service
@@ -35,15 +38,21 @@ public class BallotService {
     @Autowired
     private GeocodeController geocodeController;
 
-    public Relationship findUsernameInBallot(String winId, String username) {
-        Relationship foundBallot = dynamoDBMapper.load(Ballot.class, winId, username);
+    @Autowired
+    private UserSignInResponse userSignInResponse;
+
+    public Relationship findUsernameInBallot(String windowId, String username) {
+        String capWinId = StringUtils.capitalize(windowId);
+
+        Relationship foundBallot = dynamoDBMapper.load(Ballot.class, capWinId, username);
         if (foundBallot == null) {
             throw new ResourceNotFoundException("Ballot does not exist.");
         } 
         return foundBallot;
     }
 
-    public List<Relationship> addBallot(String username, JsonNode payload) {
+    public List<Relationship> addBallotInWindow(String windowId, String username, JsonNode payload) {
+        String capWinId = StringUtils.capitalize(windowId);
         List<Relationship> toAddRelationshipList = new ArrayList<Relationship>();
 
         Garden garden = gardenService.findGardenByGardenName(payload.get("gardenName").asText());
@@ -60,7 +69,7 @@ public class BallotService {
         String currentDate = date.format(formatter);
 
         payload.forEach(relation -> {
-            Relationship ballot = new Ballot(payload.get("winId").asText(), username, payload.get("gardenName").asText(), "Ballot" + String.valueOf(++Ballot.numInstance), currentDate, distance, "Pending");
+            Relationship ballot = new Ballot(capWinId, username, payload.get("gardenName").asText(), "Ballot" + String.valueOf(++Ballot.numInstance), currentDate, distance, "Pending");
             toAddRelationshipList.add(ballot);
         });
         
@@ -68,21 +77,40 @@ public class BallotService {
         return toAddRelationshipList;
     }
 
-    public Relationship updateGardenInBallot(String winId, String username, JsonNode payload) throws Exception {
-        Relationship toUpdateBallot = findUsernameInBallot(winId, username);
+    public Relationship updateGardenInBallot(String windowId, String username, JsonNode payload) throws Exception {
+        String capWinId = StringUtils.capitalize(windowId);
+        Relationship toUpdateBallot = findUsernameInBallot(capWinId, username);
 
         LocalDate date = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         String currentDate = date.format(formatter);
 
-        toUpdateBallot = new Ballot(winId, username, payload.get("gardenName").asText(),  "Ballot" + String.valueOf(++Ballot.numInstance), currentDate, toUpdateBallot.getDistance(), "Pending");
+        toUpdateBallot = new Ballot(capWinId, username, payload.get("gardenName").asText(),  "Ballot" + String.valueOf(++Ballot.numInstance), currentDate, toUpdateBallot.getDistance(), "Pending");
 
         dynamoDBMapper.save(toUpdateBallot);
         return toUpdateBallot;
     }      
 
-    public void deleteBallotInWindow(String winId, String username) {
-        Relationship ballotToDelete = findUsernameInBallot(winId, username);
+    public void deleteBallotInWindow(String windowId, String username) {
+        String capWinId = StringUtils.capitalize(windowId);
+
+        Relationship ballotToDelete = findUsernameInBallot(capWinId, username);
         dynamoDBMapper.delete(ballotToDelete);
+    }
+
+    public String getUsername() {
+        String idToken = userSignInResponse.getIdToken();
+        String[] chunks = idToken.split("\\."); // chunk 0 is header, chunk 1 is payload, chunk 2 is signature
+
+        Base64.Decoder decoder = Base64.getUrlDecoder(); // Decode via Base64
+        String decodedPayload = new String(decoder.decode(chunks[1])); // gets payload from idToken
+        String[] payload_attr = decodedPayload.split(",");
+
+        for (String payload : payload_attr) {
+            if (payload.contains("username")) {
+                return payload.substring(payload.indexOf(":\"") + 2, payload.length() - 1); // username is returned
+            }
+        }
+        return null;
     }
 }
