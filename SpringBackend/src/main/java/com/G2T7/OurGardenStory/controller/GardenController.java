1e7222183f1ad3f8a0739922814edb23beb04106
Goh Wei Han <weihan.goh.2021@scis.smu.edu.sg>
2022-11-04 19:46:13 +0800

    adding comments

--- SpringBackend/src/main/java/com/G2T7/OurGardenStory/controller/GardenController.java
+++ SpringBackend/src/main/java/com/G2T7/OurGardenStory/controller/GardenController.java
@@ -19,6 +19,13 @@ public class GardenController {
     @Autowired
     private GardenService gardenService;
 
+    /**
+    * Add a garden 
+    * If the garden already exists or an invalid garden is parsed, throw an exception
+    *
+    * @param garden a Garden object
+    * @return a Garden object that is newly created
+    */
     @PostMapping(path = "/garden")
     public ResponseEntity<?> saveGarden(@RequestBody Garden garden) {
         try {
@@ -30,6 +37,13 @@ public class GardenController {
         }
     }
 
+    /**
+    * Get the Garden object based on the gardenName. If garden is not found, get all gardens
+    *
+    * @param gardenName an optional String 
+    * @return a Garden object if Garden corresponding to gardenName exists,
+    *         or a list of all gardens if no gardenName is provided
+    */
     @GetMapping(path = "/garden")
     public ResponseEntity<?> findGarden(@RequestParam(name = "name") Optional<String> gardenName) {
         try {
@@ -44,6 +58,14 @@ public class GardenController {
         }
     }
 
+    /**
+    * Updates the gardenAddress and numPlots of a Garden object
+    * If the garden does not exist, throw a ResourceNotFoundException
+    *
+    * @param payload includes String gardenAddress and int numPlots
+    * @param gardenName an optional String
+    * @return the updated Garden object
+    */
     @PutMapping(path = "/garden")
     public ResponseEntity<?> updateGarden(@RequestBody JsonNode payload,
             @RequestParam(name = "name") String gardenName) {
@@ -61,6 +83,14 @@ public class GardenController {
         }
     }
 
+    /**
+    * Delete a Garden object
+    * If the gardenName parsed does not correspond to an existing Garden
+      object, throw a ResourceNotFoundException 
+    *
+    * @param gardenName an optional String
+    * @return no content
+    */
     @DeleteMapping(path = "/garden")
     public ResponseEntity<String> deleteGarden(@RequestParam(name = "name") String gardenName) {
         try {
