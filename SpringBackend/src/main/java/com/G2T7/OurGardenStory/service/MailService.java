722a5b70fa4986c274ea8f11efbafd09713b0ab7
Sean Evan Seah <seanevanseah@gmail.com>
2022-11-06 14:28:41 +0800

    merge step 1

--- SpringBackend/src/main/java/com/G2T7/OurGardenStory/service/MailService.java
+++ SpringBackend/src/main/java/com/G2T7/OurGardenStory/service/MailService.java
@@ -1,17 +1,15 @@
 package com.G2T7.OurGardenStory.service;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.*;
+import com.sendgrid.*;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
-
 import java.io.IOException;
+import java.util.*;
 
 @Service
 public class MailService {
@@ -19,19 +17,33 @@ public class MailService {
     @Value(value = "${sendgrid.api.key}")
     private String SENDGRID_API_KEY;
 
-    public String sendTextEmail (String emailTo, String status) throws IOException {
+    public String sendTextEmail(String emailTo, String username, String status, String winId_GardenName) throws IOException {
         // the sender email should be the same as we used to Create a Single Sender Verification
         Email from = new Email("jinhan.loh.2021@scis.smu.edu.sg");
         String subject = "OurGardenStory Ballot";
         Email to = new Email(emailTo);
-        String message = "";
+
+        String winId = winId_GardenName.substring(3, 4);
+        String gardenName = winId_GardenName.substring(5);
+
+        Personalization personalization = new Personalization();
+        personalization.addTo(to);
+        personalization.addDynamicTemplateData("username", username);
+        personalization.addDynamicTemplateData("winId", winId);
+        personalization.addDynamicTemplateData("gardenName", gardenName);
+        String templateId = "d-0c43bb5bdd224dd589f95d8e73a1d58f";
+
         if (status.equals("SUCCESS")) {
-            message = "Congratulations, your ballot was successful :)";
+            templateId = "d-0c43bb5bdd224dd589f95d8e73a1d58f";
         } else if (status.equals("FAIL")) {
-            message = "Sorry, your ballot was unsuccessful :(";
+            templateId = "d-a5258f55295e4addb174e782701980ac";
         }
-        Content content = new Content("text/plain", message);
-        Mail mail = new Mail(from, subject, to, content);
+
+        Mail mail = new Mail();
+        mail.setFrom(from);
+        mail.setSubject(subject);
+        mail.addPersonalization(personalization);
+        mail.setTemplateId(templateId);
 
         SendGrid sg = new SendGrid(SENDGRID_API_KEY);
         Request request = new Request();
@@ -41,8 +53,8 @@ public class MailService {
             request.setBody(mail.build());
             Response response = sg.api(request);
             return response.getBody();
-        } catch (IOException ex) {
-            throw ex;
+        } catch (IOException e) {
+            throw e;
         }
     }
 }
