1aef435c0f297d51f73a267c8dd06ca9ba86f634
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2022-11-06 21:45:37 +0800

    Fix ballot to consume refactored backend.

--- ReactFrontend/src/pages/BallotGarden/BallotGarden.tsx
+++ ReactFrontend/src/pages/BallotGarden/BallotGarden.tsx
@@ -1,69 +1,72 @@
-import { Garden, Garden as GardenType } from "models/Garden";
+import { defaultGarden, Garden as GardenType } from "models/Garden";
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router";
 import Details from "./Details/Details";
 import { BallotGardenStyled } from "./BallotGarden.styled";
 import Header from "./Header/Header";
 import Summary from "./Summary/Summary";
 import { defaultWindow, Window } from "models/Window";
 import { useParams } from "react-router-dom";
-import { Row } from "pages/BallotGardenList/Content/Content";
 import { GarWin } from "models/GarWin";
 import homeService from "service/homeService";
 import { Ballot } from "models/Ballot";
 import { AxiosError } from "axios";
 import ballotService from "service/ballotService";
+import { defaultGardenWin } from "models/GardenWin";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
 
-type Props = {};
-
-export type LinkStateType = {
-  gardenObject: GardenType;
-  numBallotsPlaced: number;
-  windowData: Window;
+export type DetailsType = {
+  numPlots: number;
+  leaseDate: string;
+  window: string;
+  status: string;
+  availablePlots: number;
+  ballotsPlaced: number;
 };
 
-export default function BallotGarden({}: Props) {
-  const [rowData, setRowData] = useState<Row[]>([]);
-  const [windowData, setWindowData] = useState<Window>(defaultWindow);
-  const [loading, setLoading] = useState(true);
+const defaultDetails: DetailsType = {
+  numPlots: 0,
+  leaseDate: "",
+  window: "",
+  status: "",
+  availablePlots: 0,
+  ballotsPlaced: 0,
+};
+
+export default function BallotGarden() {
+  const [latestWindow, setLatestWindow] = useState<Window>(defaultWindow);
+  const [garWin, setGarWin] = useState<GarWin>(defaultGardenWin);
+  const [garden, setGarden] = useState<GardenType>(defaultGarden);
+  const [ballotArr, setBallotArr] = useState<Ballot[]>([]);
   let params = useParams();
   const gardenName: string =
     params.gardenName?.replace("-", " ") ?? "Garden Not Found";
-
-  
 
   useEffect(() => {
     async function getAndSetData() {
       try {
         const latestWindow: Window = (await homeService.getLatestWindow()).data;
-        setWindowData(latestWindow);
+        setLatestWindow(latestWindow);
         const latestWinNumber: number = Number(
           latestWindow.windowId.substring(3)
         );
-        console.log(latestWindow);
-  
+
         const gardenLatestWinRelation: GarWin = (
           await ballotService.getGardenWinRelation(latestWinNumber, gardenName)
         ).data;
-  
-        console.log(gardenLatestWinRelation);
-  
-        const garden: Garden = (
+        setGarWin(gardenLatestWinRelation);
+        const garden: GardenType = (
           await homeService.getGardenByName(gardenLatestWinRelation.sk)
         ).data;
+        setGarden(garden);
 
-        console.log(garden);
-  
         const ballotInGarWinArr: Ballot[] = (
           await homeService.getAllBallotsInWindGarden(
             latestWinNumber,
             gardenLatestWinRelation.sk
           )
         ).data;
-
-        console.log(ballotInGarWinArr);
-  
-        setLoading(false);
+        setBallotArr(ballotInGarWinArr);
       } catch (e) {
         if (
           e instanceof AxiosError &&
@@ -78,14 +81,34 @@ export default function BallotGarden({}: Props) {
     getAndSetData();
   }, [gardenName]);
 
+  dayjs.extend(duration);
+
+  const winStartDate = dayjs(latestWindow.sk, "MM-DD-YYYY");
+  const windowPeriod = dayjs.duration(latestWindow.windowDuration);
+  const winEndDate = winStartDate.add(windowPeriod);
+  const windowStartEnd =
+    winStartDate.format("DD/MM/YYYY") + " - " + winEndDate.format("DD/MM/YYYY");
+
+  const leaseEndDate = winEndDate.add(dayjs.duration("P3Y"));
+  const leaseStartEnd =
+    winEndDate.format("DD/MM/YYYY") + " - " + leaseEndDate.format("DD/MM/YYYY");
+
+  const details: DetailsType = {
+    numPlots: garden.numPlots,
+    leaseDate: leaseStartEnd,
+    window: windowStartEnd,
+    availablePlots: garWin.numPlotsForBalloting,
+    status: "Open",
+    ballotsPlaced: ballotArr.length,
+  };
+
   return (
     <BallotGardenStyled>
-      {/* <Header
-        name={linkState.gardenObject.name}
-        address={linkState.gardenObject.location}
-      /> */}
+      <Header name={garden.sk} address={garden.gardenAddress} />
       <main>
-        <Summary />
+        <Summary details={details} gardenName={garden.sk} winNum={Number(
+          latestWindow.windowId.substring(3)
+        )} />
         <Details />
       </main>
     </BallotGardenStyled>
