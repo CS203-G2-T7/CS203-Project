1dcfdfc648f4ab96a7d4a9e9ee0828e302af8ae6
Wong Ginn Munn <ginnmunnwong@gmail.com>
2022-11-06 21:25:36 +0800

    added plant service and model but gets error 500

--- ReactFrontend/src/components/Navbar/RightSection/RightSection.tsx
+++ ReactFrontend/src/components/Navbar/RightSection/RightSection.tsx
@@ -8,7 +8,7 @@ export default function RightSection({}: Props) {
   return (
     <RightSectionStyled>
       <a href="/landing">Home</a>
-      <a href="/my-plants">MyPlants</a>
+      <a href="/my-plant">MyPlants</a>
       <a href="/ballot">Ballot</a>
       <a href="/gardens">Gardens</a>
       <AccountButton />
--- ReactFrontend/src/pages/MyPlants/MyPlants.tsx
+++ ReactFrontend/src/pages/MyPlants/MyPlants.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MyPlantsStyled } from "./MyPlants.styled";
 import Navbar from "components/Navbar/Navbar";
 import { NavBarStyled } from "pages/Landing/NavBar.styled";
 import { HeaderStyled } from "pages/MyPlants/Header.styled";
 import PlantCard from "./Content/PlantCard";
 import { ContentStyled } from "./Content/Content.styled";
+import plantService from "service/plantService";
 
-export type rowObject = {
-  plantName: string;
-  species: string;
+// export type rowObject = {
+//   plantName: string;
+//   species: string;
+// };
+
+export type Plant = {
+  plantSpecies: string;
+  description: string;
+  sk: string;
+  pk: string;
+};
+
+export const defaultPlant: Plant = {
+  plantSpecies: "",
+  description: "",
+  sk: "",
+  pk: "",
 };
 
 const fakeDatabase: rowObject[] = [
@@ -37,8 +52,23 @@ const fakeDatabase: rowObject[] = [
 ];
 
 type Props = {};
-
 export default function MyPlants({}: Props) {
+  const [plantDataList, setPlantDataList] = useState<Plant[]>([
+    defaultPlant,
+  ]);
+
+  useEffect(() => {
+    Promise.all([plantService.getAllUserPlants()])
+      .then((resArr) => {
+        console.log(resArr[0].data);
+        setPlantDataList(resArr[0].data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+
   return (
     <MyPlantsStyled>
       <NavBarStyled>
