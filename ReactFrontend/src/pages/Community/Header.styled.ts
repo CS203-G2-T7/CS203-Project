f0ccc95e47b0f4c1290c00b13bc2a593e821cff0
Wong Ginn Munn <ginnmunnwong@gmail.com>
2022-11-07 15:05:05 +0800

    created files for community

---
+++ ReactFrontend/src/pages/Community/Header.styled.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.div`
+  background-image: url("./image/myPlantsHeader.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  p {
+    font-family: "Roboto", serif;
+    font-weight: 400;
+    color: white;
+    margin: 0;
+    padding-left: 5rem;
+  }
+  p:nth-of-type(1) {
+    font-size: calc(38rem/16);
+    padding-top: 5rem;
+  }
+
+  p:nth-of-type(2) {
+    font-size: calc(28rem/16);
+    padding-top: 1rem;
+    padding-bottom: 10rem;
+
+  }
+`;
