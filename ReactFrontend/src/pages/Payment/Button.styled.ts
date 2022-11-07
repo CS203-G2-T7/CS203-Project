5361ed204edf55369c236d696d64f828a4b72caa
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2022-11-07 22:18:09 +0800

    Finish payment page.

---
+++ ReactFrontend/src/pages/Payment/Button.styled.ts
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+import styled from "styled-components";
+
+export const StyledButton = styled.div`
+  margin-top: 30vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ ReactFrontend/src/pages/Payment/RowGardenEntry.styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const RowGardenEntryStyled = styled.div`
+  color: #00131e;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 2.8125rem 1rem 1.5rem;
+  gap: 0.625rem;
+  span:nth-child(2) {
+    color: #9f9f9f;
+    font-size: 1rem;
+  }
+`;
