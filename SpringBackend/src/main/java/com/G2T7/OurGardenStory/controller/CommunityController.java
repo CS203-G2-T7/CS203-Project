52c60e69778436b7d49c1c1cf274a3e48bfa7c2f
haloxil <seanevanseah@gmail.com>
2022-11-08 01:14:43 +0800

    crossorgin controller

--- SpringBackend/src/main/java/com/G2T7/OurGardenStory/controller/CommunityController.java
+++ SpringBackend/src/main/java/com/G2T7/OurGardenStory/controller/CommunityController.java
@@ -12,7 +12,7 @@ import com.G2T7.OurGardenStory.service.CommunityService;
 
 import io.swagger.annotations.*;
 
-@CrossOrigin(origins = {"https://ourgardenstory.me/community", "*"})
+@CrossOrigin(origins = {"https://ourgardenstory.me/community", "*"}Z)
 @RestController
 @Api(value = "Community Controller", description = "Operations pertaining to Community model")
 public class CommunityController {
