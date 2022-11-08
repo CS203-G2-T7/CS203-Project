9e86991775293e3999c82c1abe97071772159e49
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2022-11-08 12:36:51 +0800

    Update landing check logged in to garden.

--- ReactFrontend/src/service/landingService.ts
+++ ReactFrontend/src/service/landingService.ts
@@ -1,11 +1,11 @@
 import axios, { AxiosResponse } from "axios";
 import { prod_url } from "urlConstants";
 
-const GET_WINDOW_URL = `${prod_url}/window`;
+const GET_GARDEN_URL = `${prod_url}/garden`;
 
 class Landing {
   checkLoggedIn(): Promise<AxiosResponse<any, any>> {
-    return axios.get(GET_WINDOW_URL, {
+    return axios.get(GET_GARDEN_URL, {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("jwtAccessToken"),
       },
