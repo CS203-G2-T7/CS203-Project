159e6f26d4c63f879b56188c5815728d57954c3e
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2022-11-11 15:33:10 +0800

    Implement verify in gardenservice test.

--- SpringBackend/src/test/java/com/G2T7/OurGardenStory/Garden/GardenServiceTest.java
+++ SpringBackend/src/test/java/com/G2T7/OurGardenStory/Garden/GardenServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.HashMap;
@@ -21,47 +22,47 @@ import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
 public class GardenServiceTest {
+    DynamoDBMapper mapperMock = mock(DynamoDBMapper.class);
+
     @Test
     void findAllGarden_allGarden_ReturnAllGarden() {
-        DynamoDBMapper mapperMock = mock(DynamoDBMapper.class);
         PaginatedQueryList<Garden> expected = mock(PaginatedQueryList.class);
         // Note that when needs to be completed before thenReturn can be called.
         when(mapperMock.query(eq(Garden.class), Mockito.any(DynamoDBQueryExpression.class))).thenReturn(expected);
 
         QueryService queryService = new QueryService(mapperMock);
-        PaginatedQueryList<Garden> actual = queryService.query();
+        PaginatedQueryList<Garden> actual = queryService.queryAllGardens();
 
         assertEquals(expected, actual);
+        verify(mapperMock).query(eq(Garden.class), Mockito.any(DynamoDBQueryExpression.class));
     }
 
     @Test
     void findGardenByGardenName_noSuchGarden_ReturnNull() {
-        DynamoDBMapper mapperMock = mock(DynamoDBMapper.class);
         Garden expected = mock(Garden.class);
 
-        when(mapperMock.load(eq(Garden.class),any(String.class), any(String.class)))
-            .thenReturn(expected);
+        when(mapperMock.load(eq(Garden.class), any(String.class), any(String.class)))
+                .thenReturn(expected);
 
         LoadService loadService = new LoadService(mapperMock);
         Garden actual = loadService.load("No such Garden", "No such Garden");
 
         assertEquals(expected, actual);
-    } 
+        verify(mapperMock).load(Garden.class, "No such Garden", "No such Garden");
+    }
 
     @Test
     void findGardenByGardenName_GardenPresent_ReturnGarden() {
-        DynamoDBMapper mapperMock = mock(DynamoDBMapper.class);
         Garden expected = mock(Garden.class);
 
         when(mapperMock.load(Garden.class, "Garden", "Sembawang Park"))
-            .thenReturn(expected);
+                .thenReturn(expected);
 
         LoadService loadService = new LoadService(mapperMock);
         Garden actual = loadService.load("Garden", "Sembawang Park");
-
+        verify(mapperMock).load(Garden.class, "Garden", "Sembawang Park");
         assertEquals(expected, actual);
     }
-
 }
 
 class LoadService {
@@ -71,7 +72,7 @@ class LoadService {
         this.mapper = mapper;
     }
 
-    public Garden load(String pk, String sk) {            
+    public Garden load(String pk, String sk) {
         return mapper.load(Garden.class, pk, sk);
     }
 }
@@ -83,12 +84,12 @@ class QueryService {
         this.mapper = mapper;
     }
 
-    public PaginatedQueryList<Garden> query() {
+    public PaginatedQueryList<Garden> queryAllGardens() {
         Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
         eav.put(":GDN", new AttributeValue().withS("Garden"));
         DynamoDBQueryExpression<Garden> qe = new DynamoDBQueryExpression<Garden>()
-            .withKeyConditionExpression("PK = :GDN").withExpressionAttributeValues(eav);
-        
-            return mapper.query(Garden.class, qe);
+                .withKeyConditionExpression("PK = :GDN").withExpressionAttributeValues(eav);
+
+        return mapper.query(Garden.class, qe);
     }
 }
